2c94f56ab4d5353d0ead2212f89d4b019779b83b
hongsujin2eeZyo <hong266770@gmail.com>
2025-06-27 20:36:53 +0900

    최종

--- instargram/src/pages/InstarPage.jsx
+++ instargram/src/pages/InstarPage.jsx
@@ -13,8 +13,8 @@ const imageData = [
     { src: image1, description: "📸🎞️🤓", coment: "일본에서찍은" },
     { src: image2, description: "🌀_🌀;;💦", coment: "갈머하고찍은" },
     { src: image3, description: "._.", coment: "현지랑블루본즈" },
-    { src: image4, description: "```", coment: "현지랑블루본즈2" },
-    { src: image5, description: "w", coment: "눈사람만들고집가는길에" },
+    { src: image4, description: "w", coment: "눈사람만들고집가는길에"  },
+    { src: image5, description: "```", coment: "현지랑블루본즈2" },
     { src: image6, description: "⛄️", coment: "언제왜찍었더라" },
 ];
 
